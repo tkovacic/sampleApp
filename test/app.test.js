32c284d47fc315403c6a66a26c4e68ee45583747
Tim Kovacic <kovacictim3@gmail.com>
2022-05-14 00:51:40 -0400

    init

--- test/app.test.js
+++ test/app.test.js
@@ -1,14 +1,22 @@
 const app = require('../app');
 const request = require('supertest');
+var expect = require('chai').expect();
+var expect = require('chai').should();
 
 describe('gae_node_request_example', () => {
   describe('GET /', () => {
-    it('should get 200', done => {
-      request(app).get('/').expect(200, done);
+    it('expect to get 200', () => {
+      request(app).get('/').expect(200);
     });
-
-    it('should get Hello World', done => {
-      request(app).get('/').expect('Hello, world!', done);
+    it('should return response of json', () => {
+      request(app).get('/', function(err,res,body) {
+        res.should.be.a.json;
+      });
+    });
+    it('should return json body of html', () => {
+      request(app).get('/', function(err,res,body) {
+        body.should.be.a.html;
+      });
     });
   });
 });
